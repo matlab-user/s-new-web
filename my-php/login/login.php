4ed2566936894d1cb63be7d688955376f828f2d0
blue-fish <free-bug@163.com>
2015-03-26 11:39:41 +0800

    增加了用户注册机制

--- my-php/login/login.php
+++ my-php/login/login.php
@@ -1,4 +1,6 @@
 <?php
+	session_start();
+	
 	require_once( "../php-lib/codec_lib.php" );
 	
 	$config = read_config( '../php-lib/config.cf' );
@@ -7,5 +9,45 @@
 
 	if( !( isset($_POST['user']) & isset($_POST['passwd']) ) )
 		exit;
+	
+	if( !isset($_SESSION['l_login']) )
+		$_SESSION['l_login'] = time();
+	else {
+		$now = time();
+		if( $now-$_SESSION['l_login']<3 )
+			exit;
+		else
+			$_SESSION['l_login'] = $now;
+	}
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	$sig = 'NO';
+	$sql_con = touch_mysql();
+	if( empty($sql_con) )
+		exit;
+	
+	$query_str = "SELECT passwd FROM user_table WHERE mail='".$_POST['user']."'";
+	$res = mysql_query( $query_str, $sql_con );
+	while( $row=mysql_fetch_array($res) ) {
+		if( $row[0]==md5($_POST['passwd']) ) {
+			$sig = 'devs_view.html';
+			$_SESSION['user'] = $_POST['user'];
+		}
+	};
+	mysql_close( $sql_con );
+	echo $sig;
+
+//-----------------------------------------------------------------------------
+function touch_mysql() {
+
+	global $mysql_user, $mysql_pass;
+	
+	$con = mysql_connect( 'localhost', $mysql_user, $mysql_pass );
+	if( !$con )
+		die( 'Could not connect: ' . mysql_error() );
+		
+	mysql_unbuffered_query( "SET NAMES 'utf8'", $con );
+	mysql_select_db( 'user_db', $con );
+	return $con;
+}	
 ?>
---
+++ my-php/login/register_user.php
@@ -0,0 +1,121 @@
+<?php
+	session_start();
+	
+	require_once( "../php-lib/codec_lib.php" );
+	
+	$config = read_config( '../php-lib/config.cf' );
+	$mysql_user = $config->user;
+	$mysql_pass = $config->pass;
+
+	$_POST['user'] = 'free-bug@163.c';
+	$_POST['passwd'] = 'wdh';
+	if( !( isset($_POST['user']) & isset($_POST['passwd']) ) )
+		exit;
+	
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	$sig = 'NO';
+	$sql_con = touch_mysql();
+	if( empty($sql_con) )
+		exit;
+	
+	$query_str = "SET @UID='';";
+	mysql_unbuffered_query( $query_str, $sql_con );
+	
+	$query_str = "CALL add_user_2( @UID, '".$_POST['user']."', '', '".$_POST['passwd']."' )";
+	mysql_unbuffered_query( $query_str, $sql_con );
+	
+	$res = mysql_query( "SELECT @UID;", $sql_con );
+	while( $row=mysql_fetch_array($res) ) {
+		if( $row[0]=='' )
+			$sig = 'NO';
+		else
+			$sig = $row[0];
+	}
+	mysql_close( $sql_con );
+
+	if( $sig=='NO' ) {
+		//echo "用户名以被占用，请重新输入！";
+		//return;
+	}
+	
+//----------------------------------------------------------------------------
+	$line = array();
+	$line[0] = <<<EOD
+<!DOCTYPE html>
+<html>
+    <head><meta charset="UTF-8" http-equiv="X-UA-Compatible" content="IE=edge"></head>
+</html>
+<?php
+	require_once( '../../php-lib/codec_lib.php' );
+	\$config = read_config( '../../php-lib/config.cf' );
+	\$mysql_user = \$config->user;
+	\$mysql_pass = \$config->pass;
+	
+EOD;
+	
+	$line[1] = "\$t1 = ".time().";\r\n";
+	$line[2] = "\$uid = '".$sig."';\r\n";
+	$line[3] = <<< EOD
+	\$now = time();
+	if( (\$now-\$t1)>30*60 ) { // 超时了
+		echo "注册超时，请重新注册！";
+		unlink(__FILE__);
+		return;
+	}
+		
+	\$sql_con = touch_mysql();
+	if( empty(\$sql_con) )
+		exit;
+	\$query_str = "UPDATE user_db.user_table SET state='unknown' WHERE uid='".\$uid."'";
+	mysql_unbuffered_query( \$query_str, \$sql_con );
+	mysql_close( \$sql_con );
+	
+	echo "已经完成激活，请重新登录！";
+	echo __FILE__;
+	unlink(__FILE__);
+	
+	function touch_mysql() {
+		global \$mysql_user, \$mysql_pass;
+		\$con = mysql_connect( 'localhost', \$mysql_user, \$mysql_pass );
+		if( !\$con )
+			die( 'Could not connect: ' . mysql_error() );	
+		mysql_unbuffered_query( "SET NAMES 'utf8'", \$con );
+		mysql_select_db( 'user_db', \$con );
+		return \$con;
+	}
+?>
+EOD;
+//------------------------------------------------------------------------------------
+// 产生用户帐户激活链接
+	$active_link_dir = 'active_link_dir';
+	if( !is_dir($active_link_dir) )
+		mkdir( $active_link_dir );
+	
+	$file_name = time().'-'.rand(0,time()).'-'.rand();
+	$file_name = $active_link_dir.'/'.base64_encode( $file_name ).'.php';
+	
+	$fp = fopen( $file_name, "w+" ); 
+	if( !is_writable($file_name) )
+		  die("file:" .$file_name. "Unwriteable! \r\n");
+
+	fwrite( $fp, $line[0].$line[1].$line[2].$line[3] );
+	fclose( $fp );  //关闭指针
+
+//---------------------------------------------------------------------------------
+//  发送邮件
+
+
+//-----------------------------------------------------------------------------
+function touch_mysql() {
+
+	global $mysql_user, $mysql_pass;
+	
+	$con = mysql_connect( 'localhost', $mysql_user, $mysql_pass );
+	if( !$con )
+		die( 'Could not connect: ' . mysql_error() );
+		
+	mysql_unbuffered_query( "SET NAMES 'utf8'", $con );
+	mysql_select_db( 'user_db', $con );
+	return $con;
+}	
+?>
